22c3f7d0ee72c210a9710d1c9bccc9375c0112c5
Joan Pedro <jpos.senai@gmail.com>
2020-03-28 17:30:51 -0300

    Bloqueia Query e Mutation com Authorization

---
+++ Backend/src/utils.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+function getUserId(context) {
+
+  // "Autorization": "Bearer <token_jwt>"
+  const Authorization = context.request.get('Authorization')
+  if(Authorization){
+    const token = Authorization.replace('Bearer ', '')
+    const { userId } = jwt.verify(token, process.env.JWT_SECRET)
+    return userId
+  }
+
+  throw new Error('Not authenticated!')
+
+}
+
+module.exports = { getUserId }
