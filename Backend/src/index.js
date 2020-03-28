ef2828694f157cadd8b6dd2aa1c31602b5229713
Joan Pedro <jpos.senai@gmail.com>
2020-03-28 15:24:26 -0300

    Adiciona Estrutura Modular dos Resolvers

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,18 +1,19 @@
 const { prisma } = require('./generated/prisma-client')
+const Binding = require ('prisma-binding')
 const { GraphQLServer } = require('graphql-yoga')
 
-const resolvers = {
-  Query: {
-    user(parent, args, context, info){
-      return prisma.user({ id: args.id })
-    }
-  
-  }
-} 
+const resolvers = require('./resolvers')
 
 const server = new GraphQLServer({
   typeDefs: `${__dirname}/schema.graphql`,
-  resolvers
+  resolvers,
+  context: {
+    db: new Binding.Prisma({
+      typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+      endpoint: process.env.PRISMA_ENDPOINT
+    }),
+    prisma
+  }
 })
 
 server.start().then(() => console.log('Server running on http://localhost:4000'))
