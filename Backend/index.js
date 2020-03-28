fb06d8ed3bafdd68f25591d1130cb54118b22a22
Joan Pedro <jpos.senai@gmail.com>
2020-03-28 14:07:55 -0300

    Adiciona Implementação Inicial da API

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+const { prisma } = require('./generated/prisma-client')
+const { GraphQLServer } = require('graphql-yoga')
+
+const resolvers = {
+  Query: {
+    user(parent, args, context, info){
+      return prisma.user({ id: args.id })
+    }
+  
+  }
+} 
+
+const server = new GraphQLServer({
+  typeDefs: './schema.graphql',
+  resolvers
+})
+
+server.start().then(() => console.log('Server running on http://localhost:4000'))
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Gerenciador de finanças.
 
 1. Para Criação do Container, no terminal (Linux): docker-compose up -d 
+2. Geração do Prisma-Client, no terminal (Linux): prisma generate
