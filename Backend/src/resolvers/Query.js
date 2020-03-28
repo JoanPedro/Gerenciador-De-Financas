e711e005dc5660e637e4a92bb6b531283c938444
Joan Pedro <jpos.senai@gmail.com>
2020-03-28 19:36:16 -0300

    Adiciona Query para buscar Lista de Contas

--- Backend/src/resolvers/Query.js
+++ Backend/src/resolvers/Query.js
@@ -1,5 +1,25 @@
 const { getUserId } = require('./../utils')
 
+function accounts (_, args, ctx, info){
+  const userId = getUserId(ctx)
+
+  return ctx.db.query.accounts({
+    where: {
+      OR: [
+        {
+          user: {
+            id: userId
+          }
+        },
+        {
+          user: null
+        }
+      ]
+    },
+    orderBy: 'description_ASC'
+  }, info)
+}
+
 function user (_, args, ctx, info){
   const userId = getUserId(ctx)
 
@@ -7,5 +27,6 @@ function user (_, args, ctx, info){
 }
 
 module.exports = {
+  accounts,
   user
 }
