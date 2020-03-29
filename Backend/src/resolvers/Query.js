4438589a828900fe3e156e8f8859322c37d61090
Joan Pedro <jpos.senai@gmail.com>
2020-03-29 13:56:39 -0300

    Finaliza Query SQL Adicional para Aggregation SUM!

--- Backend/src/resolvers/Query.js
+++ Backend/src/resolvers/Query.js
@@ -84,6 +84,38 @@ function records (_, { month, type, accountsIds, categoriesIds }, ctx, info){
   
 }
 
+function totalBalance (_, { date }, ctx, info){
+  const userId = getUserId(ctx)
+  const dateISO = moment(date, 'YYYY-MM-DD').endOf('day').toISOString()
+  const pgSchema = `${process.env.PRISMA_SERVICE}$${process.env.PRISMA_STAGE}`
+
+  const mutation = `
+    mutation TotalBalance($database: PrismaDatabase, $query: String!) {
+      executeRaw(database: $database, query: $query)
+    }
+  `
+  const variables = {
+    database: 'default',
+    query: `
+      SELECT SUM("${pgSchema}"."Record"."amount") as totalbalance
+        FROM "${pgSchema}"."Record"
+        
+        INNER JOIN "${pgSchema}"."_RecordToUser"
+        ON "${pgSchema}"."_RecordToUser"."A" = "${pgSchema}"."Record"."id"
+        
+        WHERE "${pgSchema}"."_RecordToUser"."B" = '${userId}'
+        AND "${pgSchema}"."Record"."date" <= '${dateISO}'
+    `
+  } // OU "" em "date"
+
+  return ctx.prisma.$graphql(mutation, variables)
+    .then(response => {
+      const totalBalance = response.executeRaw[0].totalbalance
+      return totalBalance ? totalBalance : 0
+    })
+
+}
+
 function user (_, args, ctx, info){
   const userId = getUserId(ctx)
 
@@ -94,5 +126,6 @@ module.exports = {
   accounts,
   categories,
   records,
+  totalBalance,
   user
 }
