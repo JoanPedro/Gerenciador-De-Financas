e777305d1c95bd916a407df79f12734fd197ca3f
Joan Pedro <jpos.senai@gmail.com>
2020-03-30 20:58:33 -0300

    Adiciona Apollo Client

--- frontend/src/modules/auth/views/Login.vue
+++ frontend/src/modules/auth/views/Login.vue
@@ -19,12 +19,22 @@
             dark
           >
             <v-tollbar-title>
-              Login
+              {{texts.toolbar}}
             </v-tollbar-title>
           </v-toolbar>
 
           <v-card-text>
             <v-form>
+              <v-text-field
+                v-if="!isLogin"
+                prepend-icon="person"
+                name="name"
+                label="Nome"
+                type="text"
+                :error-messages="nameErrors"
+                :success="!$v.user.name.$invalid"
+                v-model.trim="$v.user.name.$model"
+              > </v-text-field>
               <v-text-field
                 prepend-icon="email"
                 name="email"
@@ -47,9 +57,9 @@
                 block
                 depressed
                 color="secundary"
-                @click="log"
+                @click="isLogin = !isLogin"
               >
-                Criar Conta
+                {{texts.button}}
               </v-btn>
             </v-form>
           </v-card-text>
@@ -61,7 +71,7 @@
               color="primary"
               large
               @click="submit"
-            > Login </v-btn>
+            > {{texts.submiting}} </v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -75,30 +85,60 @@ import { required, email, minLength } from 'vuelidate/lib/validators'
 export default {
   name: 'Login',
   data: () => ({
+    isLogin: true,
     user: {
+      name: '',
       email: '',
       password: ''
     }
   }),
-  validations: {
-    user: {
-      email: {
-        required,
-        email
-      },
-      password: {
-        required,
-        minLength: minLength(6)
+  validations () {
+    const validations = {
+      user: {
+        email: {
+          required,
+          email
+        },
+        password: {
+          required,
+          minLength: minLength(6)
+        }
+      }
+    }
+
+    if (this.isLogin) { return validations }
+
+    return {
+      user: {
+        ...validations.user,
+        name: {
+          required,
+          minLength: minLength(3)
+        }
       }
     }
   },
   computed: {
+    texts () {
+      return this.isLogin
+        ? { toolbar: 'LOGIN', submiting: 'Entrar', button: 'Criar Conta' }
+        : { toolbar: 'CADASTRAR', submiting: 'Criar Conta', button: 'Já tenho uma conta' }
+    },
     emailErrors () {
       const errors = []
       const email = this.$v.user.email
       if (!email.$dirty) { return errors }
       !email.required && errors.push('E-mail é Obrigatório!')
       !email.email && errors.push('Insira um e-mail válido!')
+
+      return errors
+    },
+    nameErrors () {
+      const errors = []
+      const name = this.$v.user.name
+      if (!name.$dirty) { return errors }
+      !name.required && errors.push('Nome é Obrigatório!')
+      !name.minLength && errors.push(`Insira pelo menos ${name.$params.minLength.min} caracteres!`)
 
       return errors
     },
