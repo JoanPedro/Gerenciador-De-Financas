79632dd161e5ce554ddb83c7185bb7545a80beea
Joan Pedro <jpos.senai@gmail.com>
2020-04-02 17:35:59 -0300

    Adiciona (Opcional) ajuste de Push no VueRouter

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,6 +7,11 @@ import authRoutes from '@/modules/auth/router'
 import dashboardRoutes from '@/modules/dashboard/router'
 
 Vue.use(VueRouter)
+const originalPush = VueRouter.prototype.push
+
+VueRouter.prototype.push = function push (location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 const routes = [
   ...authRoutes,
