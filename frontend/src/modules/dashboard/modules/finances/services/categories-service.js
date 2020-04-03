0edcf7a17ee851938a3a7ea81103236885ea35b7
Joan Pedro <jpos.senai@gmail.com>
2020-04-02 22:28:34 -0300

    Adiciona Mutation para criação de novas Categorias

--- frontend/src/modules/dashboard/modules/finances/services/categories-service.js
+++ frontend/src/modules/dashboard/modules/finances/services/categories-service.js
@@ -1,5 +1,6 @@
 import apollo from '@/plugins/apollo'
 import CategoriesQuery from './../graphql/Categories.graphql'
+import CategoryCreateMutation from './../graphql/CategoryCreate.graphql'
 
 const categories = async ({ operation }) => {
   const res = await apollo.query({
@@ -9,6 +10,34 @@ const categories = async ({ operation }) => {
   return res.data.categories
 }
 
+const createCategory = async variables => {
+  const res = await apollo.mutate({
+    mutation: CategoryCreateMutation,
+    variables,
+    update: (proxy, { data: { createCategory } }) => {
+      try {
+        const variables = { operation: createCategory.operation }
+        const data = proxy.readQuery({
+          query: CategoriesQuery,
+          variables
+        })
+
+        data.categories = [...data.categories, createCategory]
+
+        proxy.writeQuery({
+          query: CategoriesQuery,
+          variables,
+          data
+        })
+      } catch (e) {
+        console.log('Query "categories" has not been read yet!', e)
+      }
+    }
+  })
+  return res.data.createCategory
+}
+
 export default {
-  categories
+  categories,
+  createCategory
 }
