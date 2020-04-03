32162986177a707d47dd5e75fed3c12b39b67815
Joan Pedro <jpos.senai@gmail.com>
2020-04-03 12:25:12 -0300

    Adiciona Configuração para Rota Relatórios

---
+++ frontend/src/modules/dashboard/modules/finances/views/ReportsHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Relatórios</h1>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  name: 'ReportsHome',
+  created () {
+    this.setTitle({ title: 'Relatórios ' })
+  },
+  methods: {
+    ...mapActions(['setTitle'])
+  }
+}
+</script>
+
+<style>
+</style>
