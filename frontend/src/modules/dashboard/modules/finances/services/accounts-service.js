f099804f4d5c2831a3930f4806320c168d91bdc1
Joan Pedro <jpos.senai@gmail.com>
2020-04-02 21:49:52 -0300

    Adiciona Mutattion para criar novas Contas

--- frontend/src/modules/dashboard/modules/finances/services/accounts-service.js
+++ frontend/src/modules/dashboard/modules/finances/services/accounts-service.js
@@ -1,6 +1,6 @@
 import apollo from '@/plugins/apollo'
+import AccountCreateMutation from './../graphql/AccountCreate.graphql'
 import AccountsQuery from './../graphql/Accounts.graphql'
-
 const accounts = async () => {
   const res = await apollo.query({
     query: AccountsQuery
@@ -8,6 +8,29 @@ const accounts = async () => {
   return res.data.accounts
 }
 
+const createAccount = async variables => {
+  const res = await apollo.mutation({
+    mutation: AccountCreateMutation,
+    variables,
+    update: (proxy, { data: { createAccount } }) => {
+      try {
+        const data = proxy.readQuery({
+          AccountsQuery
+        })
+        data.accounts = [...data.accounts, createAccount]
+        proxy.writeQuery({
+          query: AccountsQuery,
+          data
+        })
+      } catch (e) {
+        console.log('Query "accounts" has not been read yet!', e)
+      }
+    }
+  })
+  return res.data.createAccount
+}
+
 export default {
-  accounts
+  accounts,
+  createAccount
 }
